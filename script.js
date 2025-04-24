61ef21389a41d1a4d1021c415ce28607076a49b8
Vania Vargas <vania.vargas.igor@gmail.com>
2025-04-24 15:36:12 -0400

    terminado

---
+++ script.js
@@ -0,0 +1,9 @@
+const changeText = document.getElementById("cambioTexto");
+changeText.innerText = "Adiós";
+
+const changeColor = document.getElementById("cambioColorNaranjo");
+changeColor.style.color = "orange";
+
+function cambioColorCafe(){
+    document.getElementById("colorCafe").style.color = "brown";
+}
